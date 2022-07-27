5834f6349f14ce38c9183eaa156eb9e70a2d939d
RHC <63613510+RHC826@users.noreply.github.com>
2022-07-27 23:21:46 +0900

    update

--- hmap.py
+++ hmap.py
@@ -4,32 +4,41 @@
         - Python には dict がビルトインされているので、このプログラムには真っ二つの巻き藁の価値しかない。
         - ハッシュマップはクラスで実装する。
 """
-from typing import Optional, Union
+from typing import Union
 
 
 class ProtoHashMap:
     """
     たたき台。
-        value を投げ込むリストに key と添え字の対照表リストの二つを持ち、
+        value を投げ込むリストを持つハッシュマップ、
         key から添え字に変換することで無味乾燥な連番の記憶から解放される。
     問題
-        - ハッシュマップではない。
+        - 。
         - 探索の際、リストを線形に舐めているので非効率的。
         - 特に工夫もなくリストに放り込んでいるので、メモリをふんだんに使用する。
         - key が重複できない。value がそれぞれ異なるとき最後の value が返る
     """
 
     def __init__(self):
-        self.values: list = [False] * 100
+        self.max: int = 100
+        self.values: list = [False] * self.max
 
     def __str__(self):
         return str([f"{self.values[i]}" for i in range(len(self.values))])
+
+    def _search(self, key: str) -> int:
+        """
+        key を添え字に変換する
+        """
+        index: int = hash(key) % self.max
+
+        return index
 
     def store(self, key="", value="") -> None:
         """
         key を添え字にして value を格納する。
         """
-        index: int = _search(key)
+        index: int = self._search(key)
         if self.values[index] is not None:
             self.values[index] = value
 
@@ -39,21 +48,12 @@ class ProtoHashMap:
         """
         key を入力すると value が返る
         """
-        index = _search(key)
+        index = self._search(key)
 
         if index is not None:
             return self.values[index]
 
         return False
-
-def _search(key: str) -> Optional[int]:
-    """
-    key を添え字に変換する
-    """
-    index: Optional[int] = None
-    index = hash(key) % 100
-
-    return int(index)
 
 
 def yama_kawa_test() -> bool:
@@ -64,13 +64,14 @@ def yama_kawa_test() -> bool:
     hmp.store("yama", "kawa")
     hmp.store("きのこ", "たけのこ")
     hmp.store("foo", "bar")
-    print(hmp)
+    print(hmp.load("yama"))
 
     assert hmp.load("yama") == "kawa", "「川！」が帰っていません"
 
     print("「山！」に対して「川！」と返すテスト: yama")
     print(hmp.load("yama"))
     print(hmp.load("きのこ"))
+    print(hmp)
 
     return True
 
